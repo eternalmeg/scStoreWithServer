0ec18eb343229aaf5b5d73c921235e2a26e0a5f2
eternalmeg <eternalmegi@gmail.com>
2024-12-19 18:41:56 +0200

    fixing

--- server/index.js
+++ server/index.js
@@ -6,12 +6,15 @@ const { authMiddleWare } = require('./middlewares/authMiddleWare');
 const cookieParser = require('cookie-parser')
 const cors = require('cors');
 const { setCors } = require('./middlewares/cors')
+const path = require('path');
 
 const port=process.env.PORT || 3000;
 const localDbUrl = process.env.LOCAL_DATABASE_URL;
 const productionDb = process.env.PRODUCTION_DATABASE_URL;
 
 const app = express();
+
+app.use(express.static(path.join(__dirname, 'dist')));
 
 app.use(cors({
     origin: function (origin, callback) {
@@ -30,6 +33,9 @@ app.use(cookieParser());
 app.use(express.json());
 app.use(authMiddleWare);
 app.use(routs);
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+});
 
 
 
