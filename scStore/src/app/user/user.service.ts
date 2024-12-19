a90dd076d138268e4943655c56594a726b1dad27
eternalmeg <106076405+eternalmeg@users.noreply.github.com>
2024-12-19 21:07:09 +0200

    Update user.service.ts

--- scStore/src/app/user/user.service.ts
+++ scStore/src/app/user/user.service.ts
@@ -75,6 +75,7 @@ export class UserService implements OnDestroy {
     return this.http.post<User>(`${this.api}/auth/logout`, {})
       .pipe(tap(() => {
         this.user$$.next(undefined);
+         this.user = undefined;
         localStorage.removeItem('user')
       }))
   }
