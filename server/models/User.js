7b1a8a4714e28ae2b5977733345b16436283f8a9
eternalmeg <106076405+eternalmeg@users.noreply.github.com>
2024-12-22 11:03:36 +0200

    Update User.js

--- server/models/User.js
+++ server/models/User.js
@@ -11,7 +11,7 @@ const userSchema = new mongoose.Schema({
     email: {
         type: String,
         required: [true, 'Email is required'],
-        minLength: 10,
+        minLength: 9,
         unique: true
     },
     phone: {
