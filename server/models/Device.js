34b63d3027db36200db9a49b3663a432634e7199
eternalmeg <106076405+eternalmeg@users.noreply.github.com>
2025-01-01 12:15:49 +0200

    Update Device.js

--- server/models/Device.js
+++ server/models/Device.js
@@ -4,24 +4,26 @@ const deviceSchema = new mongoose.Schema({
     brand: {
         type: String,
         minLength: [2, 'Minimum characters 2'],
+        maxLength: [12, 'Max characters 12'],
         required: true
     },
     model: {
         type: String,
         minLength: [2, 'Minimum characters 2'],
+        maxLength: [15, 'Max characters: 15'],
         required: true
     },
     image: {
         type: String,
-        match:[/^https?:\/\/\S+$/, 'Url must start with https'],
+        match: [/^https?:\/\/\S+$/, 'Url must starts with https'],
         required: true
     },
     description: {
         type: String,
         minLength: [10, 'Minimum characters 10'],
+        maxLength: [480, 'Max characters: 480'],
         required: true
     },
-
     price: {
         type: Number,
         min: 0,
